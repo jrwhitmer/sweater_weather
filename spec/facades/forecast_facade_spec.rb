389dd95b63e4d0f2e41f26ae9d52455d7f29180f
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-11-13 19:49:49 -0800

    Create spec for forecast facade methods

---
+++ spec/facades/forecast_facade_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe ForecastFacade do
+  it 'creates current_weather objects with appropriate keys', :vcr do
+    mapquest_info = MapquestFacade.get_lat_long('denver,co')
+
+    current_weather = ForecastFacade.current_weather(mapquest_info.latitude, mapquest_info.longitude)
+
+    expect(current_weather).to be_a(CurrentWeather)
+    expect(current_weather.datetime).to be_a(DateTime)
+    expect(current_weather.sunrise).to be_a(DateTime)
+    expect(current_weather.sunset).to be_a(DateTime)
+    expect(current_weather.temperature).to be_a(Float)
+    expect(current_weather.feels_like).to be_a(Float)
+    expect(current_weather.humidity).to be_a(Float)
+    expect(current_weather.uvi).to be_a(Float)
+    expect(current_weather.visibility).to be_a(Float)
+    expect(current_weather.conditions).to be_a(String)
+    expect(current_weather.icon).to be_a(String)
+  end
+
+  xit 'creates daily weather objects with appropriate keys', :vcr do
+    mapquest_info = MapquestFacade.get_lat_long('denver,co')
+
+    daily_weathers = ForecastFacade.daily_weather(mapquest_info.latitude, mapquest_info.longitude)
+
+    expect(daily_weathers).to be_an(Array)
+    expect(daily_weathers.first).to be_a(DailyWeather)
+    expect(daily_weathers.first.date).to be_a(Date)
+    expect(daily_weathers.first.sunrise).to be_a(DateTime)
+    expect(daily_weathers.first.sunset).to be_a(DateTime)
+    expect(daily_weathers.first.max_temp).to be_a(Float)
+    expect(daily_weathers.first.min_temp).to be_a(Float)
+    expect(daily_weathers.first.conditions).to be_a(String)
+    expect(daily_weathers.first.icon).to be_a(String)
+  end
+
+  xit 'creates hourly weather objects with appropriate keys', :vcr do
+    mapquest_info = MapquestFacade.get_lat_long('denver,co')
+
+    hourly_weathers = ForecastFacade.hourly_weather(mapquest_info.latitude, mapquest_info.longitude)
+
+    expect(hourly_weathers).to be_an(Array)
+    expect(hourly_weather.first).to be_a(HourlyWeather)
+    expect(hourly_weathers.first.time).to be_a(Time)
+    expect(hourly_weathers.first.temperature).to be_a(Float)
+    expect(hourly_weathers.first.conditions).to be_a(String)
+    expect(hourly_weathers.first.icon).to be_a(String)
+  end
+end
