6c78fb8e96a10c0b44e8ed0fb3c1c8d18f15751c
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-11-13 18:55:22 -0800

    Create and test poro for mapquest endpoint

--- app/facades/mapquest_facade.rb
+++ app/facades/mapquest_facade.rb
@@ -2,7 +2,6 @@ class MapquestFacade
 
   def self.get_lat_long(location)
     data = MapquestService.request_lat_long(location)
-    require 'pry'; binding.pry
     Longlat.new(data[:results].first[:locations].first[:latLng])
   end
 
