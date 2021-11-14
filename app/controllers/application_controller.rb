850f4a7daae1f931e0c3e4071431ca300c821393
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-11-13 23:45:33 -0800

    Create new facade to centralize requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+
+  def params_exist(param)
+    param.present?
+  end
+
+  def render_invalid_params(message)
+      render json: { error: message }, status: :bad_request
+  end
 end
---
+++ spec/requests/api/v1/forecast/index_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe 'Forecast find via location query' do
+  it 'returns a forecast by location' do
+    
+  end
+end
