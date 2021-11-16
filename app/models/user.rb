b5f16b1d028a178399fb2aeebb70466287843408
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-11-16 11:23:11 -0800

    Sad path testing for post request

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-
+  validates :email, uniqueness: true, presence: true
+  validates :password, presence: true
 end
--- spec/requests/api/v1/users/create_spec.rb
+++ spec/requests/api/v1/users/create_spec.rb
@@ -17,4 +17,14 @@ RSpec.describe 'post request to users#create' do
     expect(parsed[:data][:attributes]).to have_key(:email)
     expect(parsed[:data][:attributes]).to have_key(:api_key)
   end
+
+  it 'returns an error if the user data does not get saved' do
+    json_payload = {
+      "email": "whatever@example.com",
+    }
+
+    post '/api/v1/users', params: {user: json_payload}
+
+    expect(response.status).to eq(422)
+  end
 end
